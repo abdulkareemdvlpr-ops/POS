2b3593b67e091f6dd72ae219fb73b5f9d83453fd
abdulkareemdvlpr-ops <abdulkareemdvlpr@gmail.com>
2026-07-22 14:47:27 +0500

    fix: restore native prepared statements for proper boolean mapping

--- config/database.php
+++ config/database.php
@@ -56,10 +56,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'prepare' => false,
-            'options' => [
-                \PDO::ATTR_EMULATE_PREPARES => true,
-            ],
         ],
 
     ],
